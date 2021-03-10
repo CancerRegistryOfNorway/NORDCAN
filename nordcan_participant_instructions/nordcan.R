4987b7adbc7052ed787a4715cabb9e150f258bf4
Huidong Tian <huti@krg.net>
2021-03-10 12:14:15 +0100

    Add 'last_year' for global nordcan setting

--- nordcan_participant_instructions/nordcan.R
+++ nordcan_participant_instructions/nordcan.R
@@ -123,7 +123,7 @@ nordcancore::set_global_nordcan_settings(
   stat_cancer_record_count_first_year = 1953L,
   stat_cancer_death_count_first_year = 1953L,
   regional_data_first_year = 1953L,
-  llast_year = 2019L
+  last_year = 2019L
 )
 
 # PREPROCESSING -----------------------------------------------------------
